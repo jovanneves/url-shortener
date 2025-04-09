da283a68471def6c7e2ca2db7ede830b0ac22f83
Jovan Neves <jovanneves@outlook.com>
2025-04-08 21:00:25 -0300

    Remover arquivos de build

--- .next/server/pages/_document.js
+++
@@ -1,9 +0,0 @@
-"use strict";(()=>{var e={};e.id=220,e.ids=[220],e.modules={361:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},2015:e=>{e.exports=require("react")},3873:e=>{e.exports=require("path")},8732:e=>{e.exports=require("react/jsx-runtime")},9674:(e,r,t)=>{t.r(r),t.d(r,{default:()=>d});var s=t(8732),a=t(2341);function d(){return(0,s.jsxs)(a.Html,{lang:"pt-BR",className:"dark",children:[(0,s.jsx)(a.Head,{children:(0,s.jsx)("meta",{charSet:"utf-8"})}),(0,s.jsxs)("body",{children:[(0,s.jsx)("script",{dangerouslySetInnerHTML:{__html:`
-              (function() {
-                try {
-                  // For\xe7ar modo escuro como padr\xe3o
-                  document.documentElement.classList.add('dark');
-                  localStorage.setItem('theme', 'dark');
-                } catch (e) {}
-              })();
-            `}}),(0,s.jsx)(a.Main,{}),(0,s.jsx)(a.NextScript,{})]})]})}}};var r=require("../webpack-runtime.js");r.C(e);var t=e=>r(r.s=e),s=r.X(0,[341],()=>t(9674));module.exports=s})();
